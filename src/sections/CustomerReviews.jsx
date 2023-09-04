d1ab9010d262de839fa3ff3c1ac6116b115391ea
Rohan <rohansingh684@gmail.com>
2023-09-04 11:50:54 +0530

    First Commit

---
+++ src/sections/CustomerReviews.jsx
@@ -0,0 +1,28 @@
+import { reviews } from '../constants'
+import ReviewCard from '../components/ReviewCard'
+const CustomerReviews = () => {
+  return (
+    <section className="max-container">
+      <h3 className="font-palanquin text-center font-bold text-4xl">
+        What Our <span className="text-coral-red "> Customers </span>
+        Say?
+      </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae,
+        voluptates!
+      </p>
+      <div className="mt-24 flex flex-1 justify-evenly items-center gap-14 max-lg:flex-col">
+        {reviews.map((review) => (
+          <ReviewCard
+            key={review.customerName}
+            imgURL={review.imgURL}
+            customerName={review.customerName}
+            rating={review.rating}
+            feedback={review.feedback}
+          />
+        ))}
+      </div>
+    </section>
+  )
+}
+export default CustomerReviews
---
+++ src/sections/SpecialOffer.jsx
@@ -0,0 +1,43 @@
+import { arrowRight } from '../assets/icons'
+import { offer } from '../assets/images'
+import Button from '../components/Button'
+const SpecialOffer = () => {
+  return (
+    <section className="flex justify-center items-center max-lg:flex-col-reverse gap-10 max-container">
+      <div className="flex-1 ">
+        <img
+          src={offer}
+          alt=""
+          width={773}
+          height={687}
+          className="object-contain w-full"
+        />
+      </div>
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          <span className="text-coral-red"> Special </span>
+          Offer
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab atque
+          similique, quidem quo architecto repudiandae aliquam minus rerum
+          natus? Accusantium.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque,
+          consectetur?
+        </p>
+        <div className="mt-11 flex flex-wrap gap-4">
+          <Button label="Shop Now" iconURL={arrowRight} />
+          <Button
+            label="Learn more"
+            backgroundColor="bg-white"
+            borderColor="border-slate-gray"
+            textColor="text-slate-gray"
+          ></Button>
+        </div>
+      </div>
+    </section>
+  )
+}
+export default SpecialOffer
